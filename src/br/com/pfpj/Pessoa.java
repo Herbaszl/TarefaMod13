9c3ad4ac536ca99f5fd4b7136d1d4949c5523708
João Victor Medeiros dos Santos <joaovictormedeiiros@gmail.com>
2025-05-14 17:19:35 -0300

    Segunda Commit

--- src/br/com/pfpj/Pessoa.java
+++ src/br/com/pfpj/Pessoa.java
@@ -2,4 +2,8 @@ package br.com.pfpj;
 
 public abstract class Pessoa {
 
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+	private String email;
 }
--- src/br/com/pfpj/PessoaFisica.java
+++ src/br/com/pfpj/PessoaFisica.java
@@ -1,5 +1,9 @@
 package br.com.pfpj;
 
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa {
+	
+	private Long cpf;
+	private String rg;
+	private char genero; // M ou F
 
 }
